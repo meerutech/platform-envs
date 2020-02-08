2db61014293c68ae5ddfc1b7a2c48df9761d9154
adam <adam@meerutech.com>
2020-02-08 01:02:13 +0000

    all done

--- cdk/deploy.sh
+++ cdk/deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# EXAMPLE ./deploy.sh diff
+# EXAMPLE ./deploy.sh deploy
+
 # Iterating through config.ini and passing in top level env values to use to create for each environment
 for x in $(cat config.ini |./ini2arr.py);do 
   eval $(./source_aws_env.py $x)
-  cdk diff
+  cdk $1
 done
 
